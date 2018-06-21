24281e232cce0d93bbeb63f958ca18efceaec837
songran <821687825@qq.com>
2018-06-21 13:54:32 +0800

    up

--- src/Encryption.php
+++ src/Encryption.php
@@ -20,19 +20,18 @@ class Encryption{
         // $this->priKey =  openssl_pkey_get_private(file_get_contents(__DIR__.'/../conf/openssl/rsa_private_key.pem')) ;
         
         // $this->pubKey  = openssl_pkey_get_public(file_get_contents(__DIR__.'/../conf/openssl/rsa_public_key.pem'));
-        $this->priKey  =  openssl_pkey_get_private(file_get_contents($priKeyfile));        
+        $this->priKey  = openssl_pkey_get_private(file_get_contents($priKeyfile));        
         $this->pubKey  = openssl_pkey_get_public(file_get_contents($pubKeyfile));
     }
      
-
-   
     //公钥加密
     public function  jiami($data){
         $encrypt_data = '';  
-       openssl_public_encrypt($data, $encrypt_data, $this->pubKey); 
+        openssl_public_encrypt($data, $encrypt_data, $this->pubKey); 
         $encrypt_data = base64_encode($encrypt_data);  
         return  $encrypt_data;
     }
+
     //私钥解密
     public function jiemi($encrypt_data){       
         $encrypt_data = base64_decode($encrypt_data); 
